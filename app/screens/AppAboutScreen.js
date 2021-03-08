52d124d3e58ed5771d4d187dd8430eaf918cb5fd
“Mic” <mdaizovi@gmail.com>
2021-03-08 11:48:26 +0100

    basic styling on about

--- app/screens/AppAboutScreen.js
+++ app/screens/AppAboutScreen.js
@@ -9,17 +9,19 @@ export default class AboutScreen extends React.Component {
 	render() {
 		return (
     <View style={styles.container}>
-
       <View>
         <Text style={styles.aboutText}>All Songs are streamed from US Library of Congress.
         </Text>
-      </View>
 
-      <View>
-        <Text style={styles.aboutText}>All Sounds are either CC0 1.0 public domain sounds, or recorded by me. 
+        <Text style={styles.aboutText}>All Images and Videos are CC0 1.0 public domain.
+        </Text>
+
+        <Text style={styles.aboutText}>All Sounds are either CC0 1.0 public domain sounds, 
+          or recorded by me. 
           There are a lot of skating sounds because I love to skate.
         </Text>
       </View>
+
     </View>
   );
 }
@@ -27,15 +29,16 @@ export default class AboutScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-		flex: 1,
-		backgroundColor: colors.darkGrey,
-		alignItems: 'center',
-    paddingTop:50,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+		backgroundColor: colors.lightGrey,
+    paddingHorizontal:20,
 		width:'100%',
-		alignSelf:'center'
   },
   aboutText: {
 		fontSize: 22,
 		fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    paddingVertical:20,
 	},
 });
